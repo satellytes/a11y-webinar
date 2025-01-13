4bd929b7694d81a696b1f6e532ea6da1eddeecd8
Felix Hofmann <felix.hofmann@satellytes.com>
2025-01-13 15:02:46 +0100

    chore: update modal visibility logic

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const openModalButton = document.querySelector('#open-modal');
-const modalWrapper = document.querySelector('#modal-wrapper');
+const openModalButton = document.querySelector('.open-modal');
+const modalWrapper = document.querySelector('.modal-wrapper');
 
 openModalButton.addEventListener('click', () => {
-    modalWrapper.style.visibility = 'visible';
+    modalWrapper.classList.add('modal-wrapper--show');
 })
 
-const statusIndicator = document.querySelector('#status-indicator');
+const statusIndicator = document.querySelector('.status-indicator');
 
-const disagreeButton = document.querySelector('#disagree-button');
+const disagreeButton = document.querySelector('.disagree-button');
 disagreeButton.addEventListener('click', () => {
     statusIndicator.innerHTML = 'Disagreed ❌';
     statusIndicator.style.color = 'rgb(243, 18, 96)';
-    modalWrapper.style.visibility = 'hidden';
+    modalWrapper.classList.remove('modal-wrapper--show');
 })
 
-const agreeButton = document.querySelector('#agree-button');
+const agreeButton = document.querySelector('.agree-button');
 agreeButton.addEventListener('click', () => {
     statusIndicator.innerHTML = 'Agreed ✅';
     statusIndicator.style.color = 'rgb(0,138,48)';
-    modalWrapper.style.visibility = 'hidden';
+    modalWrapper.classList.remove('modal-wrapper--show');
 })
